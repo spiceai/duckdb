ad528e1ecbbdd08550051fa101204740f29080d5
Sergei Grebnov <sergei.grebnov@gmail.com>
2025-11-13 15:07:11 -0800

    Revert size change to complete tests

--- src/include/duckdb/storage/storage_info.hpp
+++ src/include/duckdb/storage/storage_info.hpp
@@ -20,7 +20,7 @@ struct FileHandle;
 class QueryContext;
 
 //! The standard row group size
-#define DEFAULT_ROW_GROUP_SIZE 30720ULL //122880ULL
+#define DEFAULT_ROW_GROUP_SIZE 122880ULL //30720ULL
 //! The definition of an invalid block
 #define INVALID_BLOCK (-1)
 //! The maximum block id is 2^62
